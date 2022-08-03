ccbe8862746baaaabd86cf2c5914fe6e375233ee
Caio-Coldebella <c232621@dac.unicamp.br>
2022-08-03 20:52:23 -0300

    Ao cadastrar usuario os dados estão sendo enviados para a API e a o usuario redirecionado para a tela de login

--- src/modules/Register.js
+++ src/modules/Register.js
@@ -1,17 +1,29 @@
 import {useState} from "react";
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
+import axios from "axios";
 import styles from "./LoginCss";
 import logo from "../assets/logo.png";
 
 export default function Register(){
+    const navigate = useNavigate();
     const [email, setEmail] = useState("");
     const [password,setPassword] = useState("");
     const [name, setName] = useState("");
     const [photo, setPhoto] = useState("");
+    function sendregister(event){
+        event.preventDefault();
+        const postobj = {
+            email: email,
+            name: name,
+            image: photo,
+            password: password};
+        const promisse = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/sign-up",postobj);
+        promisse.then(res => {navigate("/")});
+    }
     return(
         <styles.LOGIN>
         <styles.IMAGE src={logo}/>
-        <styles.FORM>
+        <styles.FORM onSubmit={sendregister}>
             <styles.INPUT type="text" required placeholder="email" value={email} onChange={e=> setEmail(e.target.value)}/>
             <styles.INPUT type="password" required placeholder="senha" value={password} onChange={e=> setPassword(e.target.value)}/>
             <styles.INPUT type="text" required placeholder="nome" value={name} onChange={e=> setName(e.target.value)}/>
