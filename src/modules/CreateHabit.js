66e1683d882af30676abe89b2bdff013f30954c6
Caio-Coldebella <c232621@dac.unicamp.br>
2022-08-05 19:21:21 -0300

    Enquanto estiver carregando, o campo de texto e o botão devem ser desabilitados, conforme layout. Os botões dos dias da semana devem ser desabilitados, porém não é necessária mudança visual durante o loading.

--- src/modules/CreateHabit.js
+++ src/modules/CreateHabit.js
@@ -11,6 +11,7 @@ export default function CreateHabit(props){
     const [days,setDays] = useState([]);
     const [daybutton, setDaybutton] = useState([]);
     const {previous, setPrevious} = useContext(NewhabitContext);
+    const [disable, setDisable] = useState(false);
     const arrdays = ["D","S","T","Q","Q", "S","S"];
     useEffect(()=>{
         let arr = arrdays.map((item,index)=>{return <CreateHabitButton key={index} days={previous.days !== null? previous.days: days} setDays={setDays} name={item} number={index}/>;});
@@ -18,6 +19,7 @@ export default function CreateHabit(props){
     },[]);
     function sendData(event){
         event.preventDefault();
+        setDisable(true);
         const objpost = {
             name: name,
             days: days
@@ -27,22 +29,21 @@ export default function CreateHabit(props){
             }
         };
         const promisse = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits",objpost,config);
-        promisse.then(res =>{props.setNewhabit([res.data])});
-        props.setCreatehabit(null);
+        promisse.then(res =>{props.setNewhabit([res.data]);setDisable(false);props.setCreatehabit(null);});
         return null;
     }
     console.log(previous)
     return(
         <FORM onSubmit={sendData}>
             <div>
-            <INPUT type="text" required value={name} placeholder={previous.name? previous.name:"nome do hábito"} onChange={e=> {setName(e.target.value)}}/>
-            <BUTTONS>
+            <INPUT type="text" disabled={disable} required value={name} placeholder={previous.name? previous.name:"nome do hábito"} onChange={e=> {setName(e.target.value)}}/>
+            <BUTTONS disable={disable}>
                 {daybutton}
             </BUTTONS>
             </div>
             <SUBMITBUTTONS>
-                <CANCEL onClick={()=> {setPrevious({name: (name?name:previous.name), days: ((days.length>0)?days:previous.days)});props.setCreatehabit(null)}}>Cancelar</CANCEL>
-                <SAVE type="submit">Salvar</SAVE>
+                <CANCEL disabled={disable} onClick={()=> {setPrevious({name: (name?name:previous.name), days: ((days.length>0)?days:previous.days)});props.setCreatehabit(null)}}>Cancelar</CANCEL>
+                <SAVE disabled={disable} type="submit">Salvar</SAVE>
             </SUBMITBUTTONS>
         </FORM>
     );
@@ -74,6 +75,7 @@ const BUTTONS = styled.div`
     display: flex;
     height: 30px;
     width: 100%;
+    pointer-events: ${props => props.disable? "none": "all"};
 `;
 const SUBMITBUTTONS = styled.div`
     align-self: flex-end;
